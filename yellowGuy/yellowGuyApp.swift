285d130e7aa78d4f643e2452ae074b6fe63ad9b8
gogo <eric27202660@gmail.com>
2021-10-20 22:19:23 +0800

    Initial Commit

---
+++ yellowGuy/yellowGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  yellowGuyApp.swift
+//  yellowGuy
+//
+//  Created by 官鼎鈞 on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct yellowGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
